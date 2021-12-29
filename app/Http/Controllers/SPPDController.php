b9a70c6f87ab963da91b094ec85712eeb8300021
andrinur13 <andribis13@gmail.com>
2021-12-29 14:20:11 +0700

    prototype add data sppd

--- app/Http/Controllers/SPPDController.php
+++ app/Http/Controllers/SPPDController.php
@@ -7,7 +7,9 @@ use App\Models\JenisPerjalananModel;
 use App\Models\ProvinceModel;
 use App\Models\RekeningModel;
 use App\Models\SPPDModel;
+use App\Models\SubdistrictModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SPPDController extends Controller
 {
@@ -22,15 +24,74 @@ class SPPDController extends Controller
 
     public function create() {
         $data = [
+            'province_default' => 20,
+            'city_default' => 140,
+            'subdistrict_default' => 1943,
+
+            'city_asal' => CityModel::where('province_id', 20)->get(),
+            'subdistrict_asal' => SubdistrictModel::where('city_id', 140)->get(), 
+
             'province' => ProvinceModel::get(),
             'city' => CityModel::get(),
             'jenis_perjalanan' => JenisPerjalananModel::get(),
             'rekening' => RekeningModel::get()
         ];
+
         return view('sppd.create', $data);
     }
 
     public function store(Request $request) {
-        dd($request->all());
+        // $nomor_sppd = '';
+        
+        $data = [
+            'sppd_desc' => $request->sppd_desc,
+            'sppd_asal' => $request->sppd_asal_subdis,
+            'sppd_tujuan_prov' => $request->sppd_tujuan_prov,
+            'sppd_tujuan_city' => $request->sppd_tujuan_city,
+            'sppd_tujuan_subdis' => $request->sppd_tujuan_subdis,
+            'sppd_waktu' => $request->sppd_waktu,
+            'sppd_berangkat' => $request->sppd_berangkat,
+            'sppd_kembali' => $request->sppd_kembali,
+            'sppd_jenis' => $request->sppd_jenis,
+            'sppd_rek' => $request->sppd_rek,
+            'sppd_tgl' => date('Y-m-d'),
+        ];
+
+        $rules = [
+            'sppd_desc' => 'required',
+            'sppd_asal' => 'required|integer',
+            'sppd_tujuan_prov' => 'required|integer',
+            'sppd_tujuan_city' => 'required|integer',
+            'sppd_tujuan_subdis' => 'required|integer',
+            'sppd_waktu' => 'required',
+            'sppd_berangkat' => 'required|date',
+            'sppd_kembali' => 'required|date',
+            'sppd_jenis' => 'required|integer',
+            'sppd_rek' => 'required|integer',
+            'sppd_tgl' => 'required|date',
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            dd($validator);
+        }
+
+        // create kode surat
+        $month = date('m');
+        $year = date('Y');
+        $name = 'SETWAN';
+        $nomor = $month . '/' . $year . '/' . $name . '/' . '1';
+        $data['sppd_no'] = $nomor; 
+
+        SPPDModel::create($data);
+
+        // try {
+        // } catch (\Exception $th) {
+        //     return redirect('/sppd')->withErrors('error', 'error insert data');
+        // }
+
+        // return redirect('/sppd');
+
     }
 }
--- resources/views/sppd/create.blade.php
+++ resources/views/sppd/create.blade.php
@@ -27,15 +27,47 @@
                             <textarea type="text" id="sppd_desc" name="sppd_desc" class="form-control" v-model="input.sppd_desc"></textarea>
                         </div>
 
+                        <hr class="mt-4">
+
+                        <div class="font-weight-bold">
+                            Asal
+                        </div>
+
+                        {{-- <div class="text-right">
+                            <div class="btn btn-success" @click="loadAsalData()">Update</div>
+                        </div> --}}
+
                         <div class="form-group mt-4">
-                            <label for="sppd_asal">Asal</label>
+                            <label for="sppd_asal">Asal Provinsi</label>
                             <br>
-                            <select class="selectpicker" name="sppd_asal" id="sppd_asal" data-live-search="true" data-width="100%">
+                            <select class="selectpicker" name="sppd_asal_prov" id="sppd_asal_prov" data-live-search="true" data-width="100%" v-model="input.sppd_asal_prov">
                                 <option value="null">Silahkan Pilih</option>
-                                @foreach ($province as $p)
-                                    <option value="{{ $p->province_id }}"> {{ $p->province }} </option>
-                                @endforeach
+                                <option v-for="item in option.asalProv" :value="item.province_id" v-text="item.province" ></option>
                             </select>
+                        </div>
+
+                        <div class="form-group mt-4">
+                            <label for="sppd_asal">Asal Kota</label>
+                            <br>
+                            <select class="selectpicker" name="sppd_asal_city" id="sppd_asal_city" data-live-search="true" data-width="100%" v-model="input.sppd_asal_city">
+                                <option value="null">Silahkan Pilih</option>
+                                <option v-for="item in option.asalCity" :value="item.city_id" v-text="item.city_name"></option>
+                            </select>
+                        </div>
+
+                        <div class="form-group mt-4">
+                            <label for="sppd_asal">Asal Kecamatan</label>
+                            <br>
+                            <select class="selectpicker" name="sppd_asal_subdis" id="sppd_asal_subdis" data-live-search="true" data-width="100%" v-model="input.sppd_asal_subdis">
+                                <option value="null">Silahkan Pilih</option>
+                                <option v-for="item in option.asalSubdis" :value="item.subdistrict_id" v-text="item.subdistrict_name" ></option>
+                            </select>
+                        </div>
+
+                        <hr class="mt-4">
+
+                        <div class="font-weight-bold">
+                            Tujuan
                         </div>
 
                         <div class="form-group mt-4">
@@ -67,24 +99,26 @@
                             </select>
                         </div>
 
+                        <hr class="mt-4">
+
                         <div class="form-group mt-4">
                             <label for="sppd_waktu">Lama Waktu (Hari)</label>
-                            <input type="number" class="form-control" id="sppd_waktu" name="sppd_waktu">
+                            <input type="number" class="form-control" id="sppd_waktu" name="sppd_waktu" v-model="input.lama_waktu">
                         </div>
 
                         <div class="form-group mt-4">
                             <label for="sppd_berangkat">Tgl Berangkat</label>
-                            <input type="date" class="form-control" id="sppd_berangkat" name="sppd_berangkat">
+                            <input type="date" class="form-control" id="sppd_berangkat" name="sppd_berangkat" v-model="input.tgl_berangkat">
                         </div>
 
                         <div class="form-group mt-4">
                             <label for="sppd_kembali">Tgl Kembali</label>
-                            <input type="date" class="form-control" id="sppd_kembali" name="sppd_kembali">
+                            <input type="date" class="form-control" id="sppd_kembali" name="sppd_kembali" v-model="input.tgl_kembali">
                         </div>
 
                         <div class="form-group mt-4">
                             <label for="sppd_jenis">Jenis Perjalanan</label>
-                            <select name="sppd_jenis" id="sppd_jenis" class="form-control">
+                            <select name="sppd_jenis" id="sppd_jenis" class="form-control" v-model="input.jenis_perjalanan">
                                 @foreach ($jenis_perjalanan as $jp)
                                     <option value="{{ $jp->jp_id }}"> {{ $jp->jp_nama }} </option>
                                 @endforeach
@@ -95,7 +129,7 @@
                         <div class="form-group mt-4">
                             <label for="sppd_rek">Rekening</label>
                             <br>
-                            <select name="sppd_rek" id="sppd_rek" class="form-control selectpicker" data-live-search="true" data-width="100%">
+                            <select name="sppd_rek" id="sppd_rek" class="form-control selectpicker" data-live-search="true" data-width="100%" v-model="input.rekening">
                                 <option value="null">Pilih Rekening</option>
                                 @foreach ($rekening as $rk)
                                     <option value="{{ $rk->rek_id }}"> {{ $rk->rek_no . ' - ' . $rk->rek_nama }} </option>
@@ -127,18 +161,76 @@
             data: {
                 urlSite: '<?= env('APP_URL') ?>',
                 input: {
-                    sppd_des: null,
+                    sppd_desc: null,
                     sppd_tujuan_prov: "",
                     sppd_tujuan_city: "",
-                    sppd_tujuan_subdis: ""
+                    sppd_tujuan_subdis: "",
+
+                    sppd_asal_prov: '<?= $province_default ?>',
+                    sppd_asal_city: '<?= $city_default ?>',
+                    sppd_asal_subdis: '<?= $subdistrict_default ?>',
+
+                    lama_waktu: null,
+                    tgl_berangkat: null,
+                    tgl_kembali: null,
+                    jenis_perjalnan: null,
+                    rekening: null
                 },
 
                 option: {
                     city: [],
                     province: [],
-                    subdistrict: []
+                    subdistrict: [],
+
+                    asalProv: [],
+                    asalCity: [],
+                    asalSubdis: []
                 }
             },
+
+            methods: {
+                loadAsalData() {
+                    // load prov
+                    let theVue = this;
+                    fetch(`${this.urlSite}api/getallprovince`).then(response => response.json()).then(
+                        json => {
+                            this.option.asalProv = json.data;
+                            console.log(json)
+                            theVue.$nextTick(function() {
+                                $('#sppd_asal_prov').selectpicker('refresh');
+                            });
+                        }
+                    );
+
+                    // load city
+                    fetch(`${this.urlSite}api/getcityprovince/` + '<?= $province_default ?>').then(response => response.json()).then(
+                        json => {
+                            this.option.asalCity = json.data;
+                            console.log(json)
+                            theVue.$nextTick(function() {
+                                $('#sppd_asal_city').selectpicker('refresh');
+                            });
+                        }
+                    );
+
+                    fetch(`${this.urlSite}api/getsubdistrictcity/` + '<?= $city_default ?>').then(response => response.json()).then(
+                        json => {
+                            this.option.asalSubdis = json.data;
+                            console.log(json)
+                            theVue.$nextTick(function() {
+                                $('#sppd_asal_subdis').selectpicker('refresh');
+                            });
+                        }
+                    );
+
+                    
+                }
+            },
+
+            beforeMount() {
+                this.loadAsalData();
+            },
+
             watch: {
                 "input.sppd_tujuan_prov": function() {
                     let theVue = this;
@@ -173,7 +265,42 @@
                             });
                         }
                     );
-                }
+                },
+
+                "input.sppd_asal_prov": function() {
+                    let theVue = this;
+
+                    theVue.$nextTick(function() {
+                        $('#sppd_asal_city').selectpicker('refresh');
+                    });
+                    theVue.$nextTick(function() {
+                        $('#sppd_asal_subdis').selectpicker('refresh');
+                    });
+
+                    // console.log(`${this.urlSite}api/getcityprovince/${this.input.sppd_tujuan_prov}`);
+                    fetch(`${this.urlSite}api/getcityprovince/${this.input.sppd_asal_prov}`).then(response => response.json()).then(
+                        json => {
+                            this.option.asalCity = json.data;
+                            theVue.$nextTick(function() {
+                                $('#sppd_asal_city').selectpicker('refresh');
+                            });
+                        }
+                    );
+                },
+
+                "input.sppd_asal_city": function() {
+                    let theVue2 = this;
+                    // console.log(`${this.urlSite}api/getcityprovince/${this.input.sppd_asal_prov}`);
+                    fetch(`${this.urlSite}api/getsubdistrictcity/${this.input.sppd_asal_city}`).then(response => response.json()).then(
+                        json => {
+                            this.option.asalSubdis = json.data;
+                            console.log(json)
+                            theVue2.$nextTick(function() {
+                                $('#sppd_asal_subdis').selectpicker('refresh');
+                            });
+                        }
+                    );
+                },
             }
         })
     </script>
